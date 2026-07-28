6e7feec374236e467350bf31b432c8f9e43ccbf0
adhi <jaiadithiya.r@gmail.com>
2026-07-28 12:37:09 +0000

    Remove debug logs from detect-active script

--- detect-active.sh
+++ detect-active.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-set -e
-
-echo "Current directory:"
-pwd
-
-echo "Proxy folder:"
-ls -la proxy
-
-echo "Current active_backend.conf:"
-cat proxy/active_backend.conf
 
 if grep -q "frontend-blue" proxy/active_backend.conf; then
     echo "blue"
